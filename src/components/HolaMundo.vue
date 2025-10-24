a80afdd4da6bcac495d1102c5210f4a4d1cbc57e
diego.cardonahernandez <diego.cardonahernandez@iesjuandeherrera.net>
2025-10-24 09:55:38 +0200

    Primer hola mundo

---
+++ src/components/HolaMundo.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <div>
+        <h1>Mi primer Vue un Viernes, que ilusión</h1>
+    </div>
+    
+</template>
+
+<script>
+    export default  {
+        name: "HolaMundo"
+    }
+</script>
+
+<style>
+
+    h1{
+        color: "blue"
+    }
+</style>
